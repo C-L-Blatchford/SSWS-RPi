dc1548a1be5433b6ecf705cbe430b2527039e811
Charlotte Blatchford <gx23733@bristol.ac.uk>
2026-09-09 15:16:46 +0100

    Corrected analyse.py

--- analyse.py
+++ analyse.py
@@ -1,34 +1,24 @@
-#import pandas as pd
-#import matplotlib.pyplot as plt
-#import glob
-
-#files = sorted(glob.glob("ArduinoAverage*.csv"))
-
-#if len(files) == 0:
-#    raise Exception("No CSV files found")
-
-#latest = files[-1]
-#print(f"Analysing {latest}")
-
-#df = pd.read_csv(latest, skiprows=4)
-
-#plt.figure(figsize=(8,5))
-#plt.plot(df["RECORD"])
-#plt.title("Record Number")
-#plt.xlabel("Sample")
-#plt.ylabel("Record")
-#plt.grid()
-#plt.savefig("latest_plot.png")
-
-#print("Plot saved")
-
 import pandas as pd
+import matplotlib.pyplot as plt
 import glob
 
 files = sorted(glob.glob("ArduinoAverage*.csv"))
 
-latest = files[-1]
+if not files:
+    raise Exception("No CSV files found")
 
-df = pd.read_csv(latest)
+latest = files[-1]
+print(f"Analysing {latest}")
+df = pd.read_csv(latest, skiprows=[0,2,3])
 
 print(df.columns)
+
+plt.figure(figsize=(8,5))
+plt.plot(df["Batt_Volt_Avg"])
+plt.title("Battery Voltage")
+plt.xlabel("Sample")
+plt.ylabel("Voltage (V)")
+plt.grid()
+plt.savefig("battery_voltage.png")
+
+print("Plot saved")
